9d09465b5ef76654a73355b9a9cae69c0a98c8c5
aforcuti <alessandro.forcuti@gsspa.com>
2025-04-30 12:20:49 +0200

    Handled DST offset

--- illustro/Uptime/Uptime.lua
+++ illustro/Uptime/Uptime.lua
@@ -2,7 +2,8 @@ function Update()
     local bootTimestamp = tonumber(SKIN:GetMeasure("PowerOnLogonTime"):GetStringValue())
     -- if value is not yet available
     if not bootTimestamp then return "" end
-    
-    local reboot_time = os.date("%d/%m/%y %H:%M:%S", bootTimestamp)
+    -- isdst == true is Legal hour, solar otherwise    
+    local offset = os.date("*t").isdst and 3600 or 0
+    local reboot_time = os.date("%d/%m/%y %H:%M:%S", bootTimestamp + offset)
     return reboot_time
 end
